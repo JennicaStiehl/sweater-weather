5119ee4ff4b878c410db9d6f73bb63c321ec312f
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-02 15:41:57 -0600

    removes unneeded spec

--- spec/requests/api/v1/giphy_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-RSpec.describe 'Giphy API' do
-  it 'can find a snowy gif' do
-    get '/api/v1/gifs?location=denver,co'
-    results = JSON.parse(response.body, symbolize_names: true)
-    expect(response).to be_successful
-  end
-end
