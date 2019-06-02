36492db870c74b19d6458d3493298d0af1070c72
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-02 17:18:38 -0600

    adds Day model test

---
+++ spec/models/day_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+RSpec.describe Day, type: :model do
+  describe 'class methods' do
+    it 'can get attributes' do
+      attributes = {
+        icon: "snowy",
+        temperatureHigh: "34",
+        temperatureLow: "34",
+        precipProbability: "34"
+      }
+      day = Day.new(attributes)
+      expect(day.icon).to eq(attributes[:icon])
+      expect(day.high).to eq(attributes[:temperatureHigh])
+      expect(day.low).to eq(attributes[:temperatureLow])
+      expect(day.precip).to eq(attributes[:precipProbability])
+    end
+  end
+end
