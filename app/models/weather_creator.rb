592234e29cb42fc509c24bc5415bd083b4e7cef5
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-03 07:30:53 -0600

    adds all tests passing

--- app/models/weather_creator.rb
+++ app/models/weather_creator.rb
@@ -1,7 +1,7 @@
 class WeatherCreator
   attr_reader :latitude,
                     :longitude
-                    
+
   def initialize(data)
     @latitude = data.latitude
     @longitude = data.longitude
--- spec/models/weather_creator_spec.rb
+++ spec/models/weather_creator_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 RSpec.describe WeatherCreator, type: :model do
   describe 'class methods' do
-    xit 'can get attributes' do
-      attributes = {
-        latitude: "92",
-        longitude: "-142"
-      }
+    it 'can get attributes' do
+      attributes = Location.new(city: "denver,co",
+                                                latitude: "39.7392358",
+                                                longitude: "-104.990251")
+
       weather_creator = WeatherCreator.new(attributes)
-      expect(weather_creator.latitude[:latitude]).to eq(attributes[:latitude])
-      expect(weather_creator.longitude[:longitude]).to eq(attributes[:longitude])
+
+      expect(weather_creator.latitude).to eq(attributes[:latitude])
+      expect(weather_creator.longitude).to eq(attributes[:longitude])
     end
-    xit 'can get the weather' do
-      weather_creator = WeatherCreator.new("denver,co")
+    it 'can get the weather' do
+      attributes = Location.new(city: "denver,co",
+                                                latitude: "39.7392358",
+                                                longitude: "-104.990251")
+
+      weather_creator = WeatherCreator.new(attributes)
       weather = weather_creator.get_weather
       expect(weather).to be_an(Object)
     end
