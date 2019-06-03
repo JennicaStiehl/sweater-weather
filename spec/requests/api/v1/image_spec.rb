c7b4a05ac99d17c7755c1af97bd0f86b766bebc2
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-02 20:07:49 -0600

    adds image spec

---
+++ spec/requests/api/v1/image_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'Image API' do
+  it 'can search for a location related photo' do
+    get '/api/v1/photo?search=denver,co'
+    results = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(results).to be_a(Hash)
+    expect(results[:data][:attributes][:url]).to be_a(String)
+  end
+end
