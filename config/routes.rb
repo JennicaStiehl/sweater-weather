e57fb10ea08f5a36975fe5f1f378f6afede5afbd
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-05 08:21:28 -0600

    removes unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#show'
       get '/photo', to: 'photos#show'
       post '/users', to:'users#create'
-      get '/users', to: 'users#index'
       post '/sessions', to: 'sessions#create'
       post '/favorites', to: 'favorites#create'
       get '/favorites', to: 'favorites#index'
